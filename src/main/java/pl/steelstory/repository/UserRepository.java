7298583bed852252a3d418edf47ff1f5536caaed
dletkiewicz <jokerek99@gmail.com>
2024-01-14 18:17:16 +0100

    jpa repository init

--- src/main/java/pl/steelstory/repository/UserRepository.java
+++ src/main/java/pl/steelstory/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package pl.steelstory.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.steelstory.entity.UserEntity;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 }
